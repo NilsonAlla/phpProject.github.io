f9acc7164c541730214ca4e1cbf548267ece3283
NilsonAlla <103747734+NilsonAlla@users.noreply.github.com>
2022-04-17 13:39:52 +0200

    update

--- main.php
+++ main.php
@@ -16,7 +16,7 @@ include 'DB/database.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>INSPINIA | Dashboard</title>
+    <title>Nilson Alla | Dashboard</title>
 
 
     <?php
